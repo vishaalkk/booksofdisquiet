742f5432efcf1a5efda9121e37aa4599542f413f
Vishal Kella <vishal.kella@gmail.com>
2026-03-03 21:34:21 -0500

    Fix asset paths for GitHub Pages deployment

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("script.js");
     return {
+        pathPrefix: "/booksofdisquiet/",
         dir: {
             input: ".",
             output: "_site"
